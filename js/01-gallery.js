467e8506e56550918718cb7fdf7e6290a3dc8e3d
Igor Liakh <igorlyah060@gmail.com>
2023-09-15 19:19:55 +0300

    done task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js"
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery")
+
+const imgs = galleryItems.map(item => {
+	const img = `<li class="gallery__item">
+                    <a href="${item.original}" class="gallery__link" >
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-original=${item.original} loading="lazy" />
+                    </a>
+                </li>`
+	return img
+})
+
+container.innerHTML = imgs.join("")
+
+const clickHandler = e => {
+	e.preventDefault()
+	if (e.target === e.currentTarget) return
+	const instance = basicLightbox.create(`
+        <div class="modal">
+        <img src="${e.target.dataset.original}" />
+        </div>
+    `)
+	instance.show()
+}
+
+container.addEventListener("click", clickHandler)
